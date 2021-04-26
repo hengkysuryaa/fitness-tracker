2542d2aa268e36c40b66fc1c9fccf45f746e601d
hengkysuryaa <hengkysurya88@gmail.com>
2021-04-26 11:09:43 +0700

    created bottom navigation

---
+++ app/src/main/java/com/example/workout/ui/schedule/ScheduleFragment.kt
@@ -0,0 +1,38 @@
+package com.example.workout.ui.schedule
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.workout.R
+
+class ScheduleFragment : Fragment() {
+
+    private lateinit var scheduleViewModel: ScheduleViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        scheduleViewModel =
+            ViewModelProvider(this).get(ScheduleViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_schedule, container, false)
+        val textView: TextView = root.findViewById(R.id.text_schedule)
+        scheduleViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+//    override fun onActivityCreated(savedInstanceState: Bundle?) {
+//        super.onActivityCreated(savedInstanceState)
+//        viewModel = ViewModelProvider(this).get(ScheduleViewModel::class.java)
+//        // TODO: Use the ViewModel
+//    }
+
+}
---
+++ app/src/main/java/com/example/workout/ui/schedule/ScheduleViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.workout.ui.schedule
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ScheduleViewModel : ViewModel() {
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is Training Scheduler Fragment"
+    }
+    val text: LiveData<String> = _text
+}
